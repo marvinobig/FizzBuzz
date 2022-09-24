e9129e5c06694a1b8c2e44b7d4ee62142622706f
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-09-24 15:18:11 +0100

    adding a fizzbuzz challenge solved using C#

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FizzBuzz");
+            Console.Write("Enter the maximum number to check upto: ");
+            int maxNum = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= maxNum; i++)
+            {
+                if (i % 3 == 0 & i % 5 == 0) Console.WriteLine("FizzBuzz");
+                else if (i % 5 == 0) Console.WriteLine("Buzz");
+                else if (i % 3 == 0) Console.WriteLine("Fizz");
+                else Console.WriteLine(i);
+            }
+
+        }
+    }
+}
